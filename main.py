368d480c717fc20bf5d5d9766ac2e3e38a68a5d0
Matt Hebel <matthew.hebel@schulich.uwo.ca>
2016-08-09 17:01:30 -0400

    added support for spotify

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from __future__ import print_function
 import requests
 from xml.etree import ElementTree
-from HTMLParser import HTMLParser
+from html.parser import HTMLParser
 import xlwings as xw
 import time
 import sqlite3
+import datetime
+import sys
+import spotipy
+import spotipy.util as util
+import config
+
+
 
 print('Getting songs:')
+
+#put in the database
+filter_artist = ['Split Infinity Radio','The Voice of Crom House Band','Wasabi Speaks']
 
 def getInfo():
 
@@ -21,7 +31,7 @@ def getInfo():
     now_playing = parser.unescape(now_playing)
     msg = ''
     try:
-        artist,song = now_playing.split('-')
+        artist,song = now_playing.split('-',1)
         artist = artist.strip()
         song = song.strip()
         msg = logSong(artist, song)
@@ -30,15 +40,21 @@ def getInfo():
         msg = 'Wrong name format'
     finally:
         print('\r'+msg,end='')
+    if 'Added' in msg:
+        spotifyLookup(artist,song)
+        return True
+    else:
+        return False
 
 def filter(artist):
-    if artist in ['Split Infinity Radio']:
+    global filter_artist
+    if artist in filter_artist:
         return True
     else:
         return False
 
 def logSong(artist,song,own=False,downloaded=False,added_by = 'Matt'):
-    b = xw.Book('C:\\Users\\mhebel3\\Documents\\music.xls')
+    b = xw.Book('C:\\Users\\mhebel3\\Documents\\Music\\music.xls')
     sheet = b.sheets['Sheet1']
 
     if checkDuplicates(sheet,artist,song):
@@ -66,6 +82,37 @@ def checkDuplicates(sheet,artist,song):
             return True
     return False
 
+def addToPlaylist(track_id):
+    scope = 'playlist-modify-public'
+    token = util.prompt_for_user_token('thedangler', scope)
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        sp.trace = False
+        results = sp.user_playlist_add_tracks('thedangler', '1U8C7xJVJ6nlP5OXxnVPAA', [track_id])
+        print(results)
+    else:
+        print("Can't get token")
+        #"spotify:user:thedangler:playlist:1U8C7xJVJ6nlP5OXxnVPAA"
+
+
+def spotifyLookup(artist,song):
+    scope = 'user-library-read'
+    username = 'thedangler'
+    token = util.prompt_for_user_token(username,scope,config.SPOTIPY_CLIENT_ID,config.SPOTIPY_CLIENT_SECRET,config.SPOTIPY_REDIRECT_URI)
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        search_str = "artist:%s track:$s" % (artist,song)
+        result = sp.search(search_str,limit=1,type='track')
+        if len(result['tracks']['items']) > 0:
+            addToPlaylist(result['tracks']['items'][0]['id'])
+    else:
+        print("Can't get token")
+
 while True:
     getInfo()
     time.sleep(60)
+    if datetime.datetime.now().time().hour == 18:
+        exit()
+
+
+
