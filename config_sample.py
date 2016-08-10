eab9b9a4be898f804143cfa10d896573b797feb3
Matt Hebel <matthew.hebel@schulich.uwo.ca>
2016-08-10 10:22:57 -0400

    Sample config file used in main.py

---
+++ config_sample.py
@@ -0,0 +1,10 @@
+#rename file to config.py
+#spotify credentials you'll have to get from spotify developer site for web API access.
+SPOTIPY_CLIENT_ID=''
+SPOTIPY_CLIENT_SECRET=''
+SPOTIPY_REDIRECT_URI='http://example.com/spotify/callback/'
+
+#spotify username
+SPOTIFY_USER_NAME = ''
+#playlist id, I got mine from using the test rest api on developer site.
+SPOTIFY_PLAYLIST_ID = ''
