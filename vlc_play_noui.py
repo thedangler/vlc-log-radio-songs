5b2463d3532512132c16504386390cf832d4a3a5
Matt <mhebel3@uwo.ca>
2017-01-18 15:48:47 -0500

    Fixed bugs - should print notifications to the console on the same line but doesn't on windows.

--- vlc_play_noui.py
+++ vlc_play_noui.py
@@ -1,6 +1,6 @@
 import vlc
 import config
-import time
+import datetime
 import requests
 import sys
 print('Python %s' % (sys.executable))
@@ -9,7 +9,6 @@ import spotipy.util as util
 import sqlite3
 
 current_song = None
-
 
 def filter(artist):
     filter_artist = ['Split Infinity Radio', 'The Voice of Crom House Band', 'Wasabi Speaks']
@@ -22,12 +21,17 @@ def is_duplicate(song,artist):
     try:
         conn = sqlite3.connect('music.db')
         c = conn.cursor()
-        c.execute("select count(*) from music where artist = ? and song = ? and spotify = 0", (artist,song))
+        c.execute("select count(*) from music where artist = ? and song = ? and spotify = 1", (artist,song))
         (has_song,) = c.fetchone()
-        return has_song == 1
+        conn.commit()
         conn.close()
-    except:
-        print(' | Not Added to DB')
+        if has_song == 1:
+            print_out("Duplicate found : %s - %s" % (song,artist))
+        return has_song == 1
+
+    except sqlite3.Error as er:
+        print_out("DB ERROR is duplicate: ")
+        print(er)
         return False
 
 def callback(event,player):
@@ -44,6 +48,10 @@ def callback(event,player):
 
         if is_duplicate(song, artist) == False and filter(artist) == False:
             spotifyLookup(artist, song)
+
+    #emergency quit
+    if datetime.datetime.now().time().hour == config.HOUR_TO_STOP:
+        sys.exit("Program Timed Out")
 
 def spotifyAuth(username):
     scope = 'playlist-modify-private user-library-read playlist-modify-public'
@@ -62,7 +70,7 @@ def addToPlaylist(track_id):
         else:
             return False
     else:
-        print(" | Spotify - Can't get token")
+        print_out("SPOTIFY TOKEN ERROR")
 
 
 def spotifyLookup(artist,song):
@@ -81,55 +89,67 @@ def spotifyLookup(artist,song):
                     conn.commit()
                     conn.close()
                 except:
-                    print(' | Not Added to DB')
-                print("\n %s - %s  Added to Spotify Playlist" % (artist,song))
+                    print_out('DB ERROR')
+                print_out("%s - %s | Added to Spotify Playlist" % (artist,song))
         else:
-            print("\n Spotify - Track not found")
+            print_out("%s - %s | Spotify - Track not found"  % (artist,song))
     else:
-        print("\n Spotify - Can't get token")
+        print_out("Spotify - Can't get token")
+print_count = 0
+def print_out(msg):
+    global print_count
+    print_count = print_count + 1
+    if print_count == 1:
+        print('\n'+msg)
+    else:
+        print('\r'+msg)
+def main():
+    instance = vlc.Instance()
+    player_l = instance.media_list_player_new()
+    media = instance.media_list_new([config.PLS_D])
+    player_l.set_media_list(media)
+    player_l.play() # starts vlc in the background and plays the music
+    p = player_l.get_media_player()
 
-instance = vlc.Instance()
-player_l = instance.media_list_player_new()
-media = instance.media_list_new([r'D:\Users\mhebel3\Downloads\listen.pls'])
-player_l.set_media_list(media)
-player_l.play() # starts vlc in the background and plays the music
-p = player_l.get_media_player()
 
+    p = player_l.get_media_player() # get the media player
+    m = p.get_media()
 
-p = player_l.get_media_player() # get the media player
-m = p.get_media()
+    event_m = m.event_manager()
+    event_m_p = p.event_manager()
 
-event_m = m.event_manager()
-event_m_p = p.event_manager()
+    #event_m_p.event_attach(vlc.EventType.MediaPlayerTitleChanged,callback,p)
+    #event_m.event_attach(vlc.EventType.MediaMetaChanged,callback,p)
+    #event_m.event_attach(vlc.EventType.MediaParsedChanged,callback_m,m)
+    event_m_p.event_attach(vlc.EventType.MediaPlayerTimeChanged,callback,p) # stupid, don't want to use this one but its the only call back triggered
+    #event_m.event_attach(vlc.EventType.MediaStateChanged,callback)
 
-#event_m_p.event_attach(vlc.EventType.MediaPlayerTitleChanged,callback,p)
-#event_m.event_attach(vlc.EventType.MediaMetaChanged,callback,p)
-#event_m.event_attach(vlc.EventType.MediaParsedChanged,callback_m,m)
-event_m_p.event_attach(vlc.EventType.MediaPlayerTimeChanged,callback,p) # stupid, don't want to use this one but its the only call back triggered
-#event_m.event_attach(vlc.EventType.MediaStateChanged,callback)
+    q = 1
+    mute = False
+    while q == 1:
 
-q = 1
-mute = False
-while q == 1:
+        cmd = input("Enter a command: play, pause, mute, quit(q): ")
+        if cmd == "q" or cmd == "quit":
+            q = 0
+        elif cmd == "play":
+            p.play()
+        elif cmd == "pause":
+            p.pause()
+        elif cmd == "mute":
+            mute = not mute
+            if mute:
+                p.audio_set_volume(0)
+            else:
+                p.audio_set_volume(60)
 
-    cmd = input("Enter a command: play,pause,quit(q),mute: ")
-    if cmd == "q" or cmd == "quit":
-        q = 0
-    elif cmd == "play":
-        p.play()
-    elif cmd == "pause":
-        p.pause()
-    elif cmd == "mute":
-        mute = not mute
-        if mute:
-            p.audio_set_volume(0)
         else:
-            p.audio_set_volume(60)
+            pass
 
-    else:
-        pass
+    return
 
-exit(1)
+
+if __name__ == '__main__':
+    main()
 
 
 
